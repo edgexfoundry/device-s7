5a0e56abbfca6905b131fbeb89cd9f5434d965c8
Jieke Choo <jiekechoo@gmail.com>
2023-10-18 18:59:11 +0800

    Initial code commit for EdgeX 3.x
    Signed-off-by: Jieke Choo <jiekechoo@gmail.com>

---
+++ internal/driver/utils.go
@@ -0,0 +1,34 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2021 IOTech Ltd
+// Copyright (C) 2022 Schneider Electric
+// Copyright (C) 2023 YIQISOFT
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package driver
+
+import (
+	"fmt"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/errors"
+	"github.com/spf13/cast"
+)
+
+func castToInt(i interface{}, field string) (int, errors.EdgeX) {
+	res, err := cast.ToIntE(i)
+	if err != nil {
+		return 0, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("%s should be castable to an integer value", field), err)
+	}
+
+	return res, nil
+}
+
+func castToString(i interface{}) (string, errors.EdgeX) {
+	res, err := cast.ToStringE(i)
+	if err != nil {
+		return "", errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("should be castable to a string value"), err)
+	}
+
+	return res, nil
+}
