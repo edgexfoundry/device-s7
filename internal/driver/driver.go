bec349e2f9af90d5dee38f24426ea0dece7bc46d
bruce <weichou1229@gmail.com>
2024-10-30 15:59:53 +0800

    refactor!: update module to v4
    BREAKING CHANGE: update go module to v4
    
    Signed-off-by: bruce <weichou1229@gmail.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -15,11 +15,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/edgexfoundry/device-sdk-go/v3/pkg/interfaces"
-	sdkModel "github.com/edgexfoundry/device-sdk-go/v3/pkg/models"
-	"github.com/edgexfoundry/go-mod-core-contracts/v3/clients/logger"
-	"github.com/edgexfoundry/go-mod-core-contracts/v3/common"
-	"github.com/edgexfoundry/go-mod-core-contracts/v3/models"
+	"github.com/edgexfoundry/device-sdk-go/v4/pkg/interfaces"
+	sdkModel "github.com/edgexfoundry/device-sdk-go/v4/pkg/models"
+	"github.com/edgexfoundry/go-mod-core-contracts/v4/clients/logger"
+	"github.com/edgexfoundry/go-mod-core-contracts/v4/common"
+	"github.com/edgexfoundry/go-mod-core-contracts/v4/models"
+
 	"github.com/robinson/gos7"
 	"github.com/spf13/cast"
 )
